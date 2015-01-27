ff0cd5739c7748cb300c021250ebb52b27c42303
eulerworks <eulerworks@gmail.com>
2015-01-27 13:18:52 -0800

    Moved Read/write functions

--- memory.c
+++ memory.c
@@ -5,8 +5,60 @@
 ** 21 JANUARY 2015
 ** MEMORY.C 	IMPLEMENTATION FILE FOR MEMORY OP FUNCTIONS
 ******************************************************************************/
+#include "memory.h"
+/******************************************************************************
+** 	READ FROM MEMORY
+******************************************************************************/
+uint16_t mem_read(uint16_t to_convert){
+	uint8_t page;
+	uint8_t offset;
+	uint16_t converted;
+	//parse address from CPMA
+	page = (0b0000111110000000 & to_convert);//0xF80
+	offset = (0b0000000001111111 & to_convert);//0x7F
+	converted = (page + offset);
+	#ifdef DEBUG
+		printf("UNCONVERTED:\n");
+		printf("Address:\n\tHEX: 0x%x\tOCTAL: %o\tDEC: %lu\n",to_convert,to_convert,to_convert);
+		printf("CONVERTED:\n");
+		printf("Address:\n\tHEX: 0x%x\tOCTAL: %o\tDEC: %lu\n",converted,converted,converted);
+		printf("Page:\n\tHEX: 0x%x\tOCTAL: %o\tDEC: %lu\n",page,page,page);
+		printf("Offset:\n\tHEX: 0x%x\tOCTAL: %o\tDEC: %lu\n",offset,offset,offset);
+	#endif
+	//access memory at address in array
+	//place read data in MB
+	return memory[converted];
+}/*end mem_read()*/
 
-//TODO: UM, YA, ADD SOME CODE....
+/******************************************************************************
+** 	WRITE TO MEMORY 	
+******************************************************************************/
+void mem_write(uint16_t to_convert, uint16_t data){
+	uint8_t page;
+	uint8_t offset;
+	uint16_t converted;
+	//parse address from CPMA
+	page = (0b0000111110000000 & to_convert);//0xF80
+	offset = (0b0000000001111111 & to_convert);//0x7F
+	converted = (page + offset);
+	#ifdef DEBUG
+		printf("UNCONVERTED:\n");
+		printf("Address:\n\tHEX: 0x%x\tOCTAL: %o\tDEC: %lu\n",to_convert,to_convert,to_convert);
+		printf("CONVERTED:\n");
+		printf("Address:\n\tHEX: 0x%x\tOCTAL: %o\tDEC: %lu\n",converted,converted,converted);
+		printf("Page:\n\tHEX: 0x%x\tOCTAL: %o\tDEC: %lu\n",page,page,page);
+		printf("Offset:\n\tHEX: 0x%x\tOCTAL: %o\tDEC: %lu\n",offset,offset,offset);
+	#endif
+
+	//go to location in memory array
+	memory[converted] = data;
+
+	#ifdef DEBUG
+		printf("CALLEE->WROTE: %o to: %o IN OCTAL\n", data, converted);
+		printf("CALLEE->WROTE: %lu to: %lu IN UNSIGNED DEC\n", data, converted);
+	#endif
+}/*end mem_write*/
+
 
 
 /******************************************************************************
